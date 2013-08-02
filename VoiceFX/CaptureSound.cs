50b0c9e16a2d3179d844f6c9ffa53c813b549211
Veggie <cmderochie+github@gmail.com>
2013-08-02 13:14:41 -0600

    Initial commit.

---
+++ VoiceFX/CaptureSound.cs
@@ -0,0 +1,368 @@
+﻿using System;
+using System.Linq;
+using SlimDX;
+using SlimDX.DirectSound;
+using SlimDX.Multimedia;
+using System.IO;
+using System.Threading;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using MultithreadedStream;
+using System.Numerics;
+
+namespace VoiceFX
+{
+    class Program
+    {
+        [System.Runtime.InteropServices.DllImport("user32.dll", CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+        static extern short GetAsyncKeyState(int key);
+        const int VK_ESCAPE = 0x1B;
+
+        static void Main(string[] args)
+        {
+            #region Capture
+            WaveFormat format = new WaveFormat();
+            format.FormatTag = WaveFormatTag.Pcm;
+            format.Channels = 1;
+            format.BitsPerSample = 16;
+            format.SamplesPerSecond = 32768;
+            format.BlockAlignment = (short)(format.BitsPerSample / 8);
+            format.AverageBytesPerSecond = format.SamplesPerSecond * format.BlockAlignment * format.Channels;
+
+            //FileStream stream = File.Open("output.raw", FileMode.OpenOrCreate);
+            MemoryStreamMultiplexer stream = new MemoryStreamMultiplexer();
+
+            SoundBufferDescription primaryBufferDescription = new SoundBufferDescription();
+            primaryBufferDescription.Format = format;
+            primaryBufferDescription.Flags = BufferFlags.GlobalFocus;
+            primaryBufferDescription.SizeInBytes = 2 * format.AverageBytesPerSecond;
+
+            // Primary Buffer
+            DirectSound ds = new DirectSound();//DirectSoundGuid.DefaultVoiceCaptureDevice);
+            ds.SetCooperativeLevel(System.Diagnostics.Process.GetCurrentProcess().MainWindowHandle, CooperativeLevel.Normal);
+
+            // Capture
+            CaptureBufferDescription captureBufferDescription = new CaptureBufferDescription()
+            {
+                Format = format,
+                BufferBytes = format.AverageBytesPerSecond,
+                WaveMapped = true,
+                ControlEffects = false,
+            };
+            DirectSoundCapture directSoundCapture = new DirectSoundCapture(DirectSoundGuid.DefaultVoiceCaptureDevice);
+            CaptureBuffer captureBuffer = new CaptureBuffer(directSoundCapture, captureBufferDescription);
+
+            const int bufferPortionCount = 8;
+            int bufferPortionSize = captureBuffer.SizeInBytes / bufferPortionCount;
+
+            List<NotificationPosition> notifications = new List<NotificationPosition>();
+            for (int i = 0; i < bufferPortionCount; i++)
+            {
+                notifications.Add(new NotificationPosition()
+                {
+                    Offset = bufferPortionSize - 1 + bufferPortionSize * i,
+                    Event = new AutoResetEvent(false),
+                });
+            }
+            captureBuffer.SetNotificationPositions(notifications.ToArray());
+            WaitHandle[] waitHandles = new WaitHandle[notifications.Count];
+            for (int i = 0; i < notifications.Count; i++)
+            {
+                waitHandles[i] = notifications[i].Event;
+            }
+
+            captureBuffer.Start(true);
+            #endregion
+
+            #region Output
+            
+            DirectSound ds2 = new DirectSound(DirectSoundGuid.DefaultPlaybackDevice);
+            ds2.SetCooperativeLevel(System.Diagnostics.Process.GetCurrentProcess().MainWindowHandle, CooperativeLevel.Priority);
+
+            SoundBufferDescription outDesc = new SoundBufferDescription()
+            {
+                Format = format,
+                Flags = BufferFlags.GlobalFocus,
+                SizeInBytes = 4 * format.AverageBytesPerSecond
+            };
+
+            PrimarySoundBuffer pBuffer = new PrimarySoundBuffer(ds2, outDesc);
+
+            SoundBufferDescription outDesc2 = new SoundBufferDescription()
+            {
+                Format = format,
+                Flags = BufferFlags.GlobalFocus | BufferFlags.ControlPositionNotify | BufferFlags.GetCurrentPosition2,
+                SizeInBytes = format.AverageBytesPerSecond / 2
+            };
+
+            SecondarySoundBuffer sBuffer1 = new SecondarySoundBuffer(ds2, outDesc2);
+
+            NotificationPosition[] notifications2 = new NotificationPosition[2];
+            notifications2[0].Offset = outDesc2.SizeInBytes / 2 + 1;
+            notifications2[1].Offset = outDesc2.SizeInBytes - 1; ;
+
+            notifications2[0].Event = new AutoResetEvent(false);
+            notifications2[1].Event = new AutoResetEvent(false);
+            sBuffer1.SetNotificationPositions(notifications2);
+
+            byte[] bytes1 = new byte[outDesc2.SizeInBytes / 2];
+            byte[] bytes2 = new byte[outDesc2.SizeInBytes];
+
+            Thread fillBuffer = new Thread(() =>
+            {
+                try
+                {
+                    var reader = stream.GetReader();
+                    int readNumber = 1;
+                    int bytesRead;
+
+                    bytesRead = reader.Read(bytes2, 0, outDesc2.SizeInBytes);
+                    sBuffer1.Write<byte>(bytes2, 0, LockFlags.None);
+                    sBuffer1.Play(0, PlayFlags.Looping);
+                    while (true)
+                    {
+                        //if (bytesRead == 0) { break; }
+                        notifications2[0].Event.WaitOne();
+                        bytesRead = reader.Read(bytes1, 0, bytes1.Length);
+                        sBuffer1.Write<byte>(bytes1, 0, LockFlags.None);
+
+                        //if (bytesRead == 0) { break; }
+                        notifications2[1].Event.WaitOne();
+                        bytesRead = reader.Read(bytes1, 0, bytes1.Length);
+                        sBuffer1.Write<byte>(bytes1, outDesc2.SizeInBytes / 2, LockFlags.None);
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Thread quit.");
+                }
+                finally
+                {
+                    stream.Dispose();
+                }
+            });
+            fillBuffer.Start();
+            #endregion
+
+            #region Init
+            var bufferPortion = new byte[bufferPortionSize];
+            var i16bufPortion = new Int16[bufferPortionSize / sizeof(Int16)];
+            var i16bufPortion2 = new Int16[bufferPortionSize / sizeof(Int16)];
+            var dblbufPortion = new double[2 * bufferPortionSize / sizeof(Int16)];
+            uint sampleRate = (uint)format.SamplesPerSecond;// (uint)Math.Pow(2, Math.Ceiling(Math.Log(i16bufPortion.Length, 2)));
+            vector = new double[2 * sampleRate];
+            var vec2 = new double[2 * sampleRate];
+            int bufferPortionIndex;
+            DateTime lastNotificationTime = DateTime.MinValue;
+            DateTime now;
+            var fake = new float[i16bufPortion.Length];
+            for (int i = 0; i < fake.Length; i++)
+            {
+                if (i % 40 < 11)
+                    fake[i] = -1f / 4;
+                else if (i % 40 < 21)
+                    fake[i] = 0f;
+                else if (i % 40 < 31)
+                    fake[i] = 1f / 4;
+                else
+                    fake[i] = 0f;
+            }
+            #endregion
+
+            while (GetAsyncKeyState(VK_ESCAPE) == 0)
+            {
+                bufferPortionIndex = WaitHandle.WaitAny(waitHandles);
+
+                captureBuffer.Read<byte>(bufferPortion, 0, bufferPortionSize, bufferPortionSize * bufferPortionIndex);
+
+                now = DateTime.Now;
+                System.Diagnostics.Debug.Write(String.Format("{0}\t{1}\n", bufferPortionIndex, (now - lastNotificationTime).Milliseconds));
+                lastNotificationTime = now;
+
+                Buffer.BlockCopy(bufferPortion, 0, i16bufPortion, 0, bufferPortion.Length);
+                for (int i = 0; i < i16bufPortion.Length; i++)
+                {
+                    dblbufPortion[2 * i] = i16bufPortion[i] / -(double)Int16.MinValue;
+                    dblbufPortion[2 * i + 1] = 0;
+                }
+                ComplexFFT(dblbufPortion, (ulong)i16bufPortion.Length, sampleRate / 2, 1);
+                /*for (int f = 0; f < 200; f++)
+                {
+                    vec2[2 * f] = vector[2 * f];
+                    vec2[2 * f + 1] = vector[2 * f + 1];
+                }
+                for (int f = 200; f < sampleRate / 2; f++)
+                {
+                    vec2[2 * f] = 0;
+                    vec2[2 * f + 1] = 0;
+                }
+                for (int f = 100; f < sampleRate / 4; f++)
+                {
+                    /*Complex c1 = new Complex(vector[4 * f - 2], vector[4 * f - 1]);
+                    Complex c2 = new Complex(vector[4 * f + 2], vector[4 * f + 3]);
+                    Complex c3 = new Complex(vector[4 * f], vector[4 * f + 1]);
+                    Complex c4 = Complex.Sqrt(c1 * c2);
+                    Complex c5 = Complex.Sqrt(c3 * c4);
+
+                    vec2[2 * f] += c5.Real;
+                    vec2[2 * f + 1] += c5.Imaginary;* /
+                    vec2[2 * f] += vector[4 * f] + (vector[4 * f - 2] + vector[4 * f + 2]) / 2;
+                    vec2[2 * f + 1] += vector[4 * f + 1] + (vector[4 * f - 1] + vector[4 * f + 3]) / 2;
+                }
+                for (int f = (int)sampleRate / 2; f < sampleRate / 2; f++)
+                {
+                    vec2[2 * f] = vector[2 * f];
+                    vec2[2 * f + 1] = vector[2 * f + 1];
+                }/**/
+                for (int f = 0; f < sampleRate / 2; f++)
+                {
+                    vec2[f] = vector[f];
+                    vec2[f + sampleRate / 2] = 0;
+                    //vec2[vec2.Length - 2 - 2 * f] = 0;// vec2[2 * f];// vector[f] / 2;
+                    //vec2[vec2.Length - 1 - 2 * f] = 0;// vec2[2 * f + 1];
+                }/**/
+                ComplexFFT(vec2, (ulong)sampleRate / 2, sampleRate, -1);
+                for (int i = 0; i < i16bufPortion.Length; i++)
+                {
+                    double mag = vector[2 * i] / (sampleRate);
+                    if (mag > 1)
+                        mag = 1;
+                    else if (mag < -1)
+                        mag = -1;
+                    //i16bufPortion[i] /= 8;
+                    i16bufPortion[i] = (Int16)(mag * -(double)Int16.MinValue + 0.5);
+                    //i16bufPortion[i] += (Int16)(i16bufPortion2[i] / 2);
+                }
+                Buffer.BlockCopy(i16bufPortion, 0, bufferPortion, 0, bufferPortion.Length);
+                Buffer.BlockCopy(i16bufPortion, 0, i16bufPortion2, 0, bufferPortion.Length);
+                //double sum = i16bufPortion.Sum((i16) => ((double)i16));
+                //if (sum == 0)
+                    //Buffer.BlockCopy(fake, 0, bufferPortion, 0, bufferPortion.Length);
+
+                stream.Write(bufferPortion, 0, bufferPortion.Length);
+            }
+
+            captureBuffer.Stop();
+            fillBuffer.Abort();
+
+            for (int i = 0; i < notifications.Count; i++)
+            {
+                notifications[i].Event.Close();
+            }
+
+            captureBuffer.Dispose();
+            directSoundCapture.Dispose();
+            ds.Dispose();
+
+        }
+
+        static void SWAP(ref double d1, ref double d2)
+        {
+            double temp = d1;
+            d1 = d2;
+            d2 = temp;
+        }
+
+        static double[] vector;
+        
+        static void ComplexFFT(double[] data, ulong number_of_samples, uint sample_rate, int sign)
+        {
+
+	        //variables for the fft
+	        ulong n,mmax,m,j,istep,i;
+	        double wtemp,wr,wpr,wpi,wi,theta,tempr,tempi;
+
+	        //the complex array is real+complex so the array 
+            //as a size n = 2* number of complex samples
+            //real part is the data[index] and 
+            //the complex part is the data[index+1]
+
+	        //new complex array of size n=2*sample_rate
+            //double[] vector = data;// new double[2 * sample_rate];
+
+	        //put the real array in a complex array
+	        //the complex part is filled with 0's
+	        //the remaining vector with no data is filled with 0's
+	        for(n=0; n<sample_rate;n++)
+	        {
+                if (n < number_of_samples)
+                {
+                    vector[2 * n] = data[2 * n];
+                    vector[2 * n + 1] = data[2 * n + 1];
+                }
+                else
+                {
+                    vector[2 * n] = 0;
+                    vector[2 * n + 1] = 0;
+                }
+	        }
+
+	        //binary inversion (note that the indexes 
+            //start from 0 witch means that the
+            //real part of the complex is on the even-indexes 
+            //and the complex part is on the odd-indexes)
+	        n=sample_rate << 1;
+	        j=0;
+	        for (i=0;i<n/2;i+=2) {
+		        if (j > i) {
+			        SWAP(ref vector[j],ref vector[i]);
+                    SWAP(ref vector[j + 1], ref vector[i + 1]);
+			        if((j/2)<(n/4)){
+                        SWAP(ref vector[(n - (i + 2))], ref vector[(n - (j + 2))]);
+                        SWAP(ref vector[(n - (i + 2)) + 1], ref vector[(n - (j + 2)) + 1]);
+			        }
+		        }
+		        m=n >> 1;
+		        while (m >= 2 && j >= m) {
+			        j -= m;
+			        m >>= 1;
+		        }
+		        j += m;
+	        }
+	        //end of the bit-reversed order algorithm
+
+	        //Danielson-Lanzcos routine
+	        mmax=2;
+	        while (n > mmax) {
+		        istep=mmax << 1;
+		        theta=sign*(2*Math.PI/mmax);
+		        wtemp=Math.Sin(0.5*theta);
+		        wpr = -2.0*wtemp*wtemp;
+		        wpi=Math.Sin(theta);
+		        wr=1.0;
+		        wi=0.0;
+		        for (m=1;m<mmax;m+=2) {
+			        for (i=m;i<=n;i+=istep) {
+				        j=i+mmax;
+				        tempr=wr*vector[j-1]-wi*vector[j];
+				        tempi=wr*vector[j]+wi*vector[j-1];
+				        vector[j-1]=vector[i-1]-tempr;
+				        vector[j]=vector[i]-tempi;
+				        vector[i-1] += tempr;
+				        vector[i] += tempi;
+			        }
+			        wr=(wtemp=wr)*wpr-wi*wpi+wr;
+			        wi=wi*wpr+wtemp*wpi+wi;
+		        }
+		        mmax=istep;
+	        }
+	        //end of the algorithm
+	
+	        /*
+            //determine the fundamental frequency
+	        //look for the maximum absolute value in the complex array
+	        fundamental_frequency=0;
+	        for(i=2; i<=sample_rate; i+=2)
+	        {
+		        if((pow(vector[i],2)+pow(vector[i+1],2))>(pow(vector[fundamental_frequency],2)+pow(vector[fundamental_frequency+1],2))){
+			        fundamental_frequency=i;
+		        }
+	        }
+
+	        //since the array of complex has the format [real][complex]=>[absolute value]
+	        //the maximum absolute value must be ajusted to half
+	        fundamental_frequency=(long)floor((float)fundamental_frequency/2);
+            */
+        }
+    }
+}
---
+++ VoiceFX/MemoryStreamMultiplexer.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.IO;
+
+namespace MultithreadedStream
+{
+    /// <summary>
+    /// Multithreaded buffer where one thread can write and many threads can read simultaneously. 
+    /// </summary>
+    public class MemoryStreamMultiplexer : IDisposable
+    {
+        private ManualResetEvent[] _dataReadyEvents = new ManualResetEvent[255];
+        private ManualResetEvent[] _finishedEvents = new ManualResetEvent[255];
+        private int _readerCount = 0;
+
+        private bool _finished;
+        private int _Length;
+        private List<byte[]> _Buffer = new List<byte[]>();
+
+        public int Length { get { return _Length; } }
+
+        public MemoryStreamMultiplexer()
+        {
+
+        }
+
+        public void Write(byte[] data, int pos, int length)
+        {
+            byte[] newBuf = new byte[length];
+            Buffer.BlockCopy(data, pos, newBuf, 0, length);
+            lock (_Buffer)
+            {
+                _Buffer.Add(newBuf);
+                _Length += length;
+            }
+            Set();
+        }
+
+        private void Set()
+        {
+            for (int i = 0; i < _readerCount; i++)
+                _dataReadyEvents[i].Set();
+        }
+
+        public void Finish()
+        {
+            for (int i = 0; i < _readerCount; i++)
+                _finishedEvents[i].Set();
+            _finished = true;
+        }
+
+        public MemoryStreamReader GetReader()
+        {
+            ManualResetEvent dataReady = new ManualResetEvent(_finished);
+            ManualResetEvent finished = new ManualResetEvent(_finished);
+            lock (_dataReadyEvents)
+            {
+                _dataReadyEvents[_readerCount] = dataReady;
+                _finishedEvents[_readerCount] = finished;
+                _readerCount++;
+            }
+            return new MemoryStreamReader(_Buffer, dataReady, finished);
+        }
+
+        private bool disposed = false;
+        public void Dispose()
+        {
+            if (!disposed)
+            {
+                Finish();
+
+                for (int i = 0; i < _readerCount; i++)
+                {
+                    _dataReadyEvents[i].Dispose();
+                    _finishedEvents[i].Dispose();
+                }
+                _readerCount = 0;
+
+                disposed = true;
+            }
+        }
+    }
+
+    public class MemoryStreamReader : Stream, IDisposable
+    {
+        private int _position;
+        private int _bufferIndex;
+        private int _bufferPos;
+        private List<byte[]> _bufferList;
+
+        private ManualResetEvent[] _waitHandles;
+        private ManualResetEvent _dataReady;
+        private ManualResetEvent _finished;
+
+        public MemoryStreamReader(List<byte[]> bufferList, ManualResetEvent dataReady, ManualResetEvent finished)
+        {
+            _waitHandles = new ManualResetEvent[] { dataReady, finished };
+            _bufferList = bufferList;
+            _dataReady = dataReady;
+            _finished = finished;
+            _bufferPos = 0;
+            _bufferIndex = 0;
+            _position = 0;
+        }
+
+        public override bool CanRead
+        {
+            get { return true; }
+        }
+
+        public override bool CanSeek
+        {
+            get { return false; }
+        }
+
+        public override bool CanWrite
+        {
+            get { return false; }
+        }
+
+        public override void Flush()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long Length
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override long Position
+        {
+            get
+            {
+                return _position;
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            if (_bufferIndex < _bufferList.Count)
+            {
+                return ReadInternal(buffer, offset, count);
+            }
+            else
+            {
+                _dataReady.Reset();
+                // Wait for either data ready event of the finished event.
+                int index = WaitHandle.WaitAny(_waitHandles, TimeSpan.FromSeconds(30), false);
+                // either of the event fired. see if there's more data to read.
+                if (_bufferIndex < _bufferList.Count)
+                    return ReadInternal(buffer, offset, count);
+                else
+                    return 0;   // No more bytes will be available. Finished.
+            }
+        }
+
+        private int ReadInternal(byte[] buffer, int offset, int count)
+        {
+            byte[] currentBuffer = _bufferList[_bufferIndex];
+
+            if (_bufferPos + count <= currentBuffer.Length)
+            {
+                // the current buffer holds the same or more bytes than what is asked for
+                // So, give what was asked.
+                Buffer.BlockCopy(currentBuffer, _bufferPos, buffer, offset, count);
+
+                _bufferPos += count;
+                _position += count;
+                return count;
+            }
+            else
+            {
+                // current buffer does not have the necessary bytes. deliver whatever is available.
+                if (_bufferPos < currentBuffer.Length)
+                {
+                    int remainingBytes = currentBuffer.Length - _bufferPos;
+                    Buffer.BlockCopy(currentBuffer, _bufferPos, buffer, offset, remainingBytes);
+
+                    _position += remainingBytes;
+                    _bufferIndex++;
+                    _bufferPos = 0;
+
+                    // Try to read from the next buffer in the list and deliver
+                    // the undelivered bytes. The Read call might block and wait for 
+                    // remaining bytes to appear. 
+                    return remainingBytes +
+                        this.Read(buffer, offset + remainingBytes, count - remainingBytes);
+                }
+                else
+                {
+                    // Already all bytes from currnet buffer has been delivered. Try next buffer.
+                    _bufferIndex++;
+                    _bufferPos = 0;
+
+                    // There may not be next buffer and thus we will have to wait.
+                    return this.Read(buffer, offset, count);
+                }
+            }
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public new void Dispose()
+        {
+            _dataReady = null;
+            _finished = null;
+            _bufferList = null;
+            _waitHandles = null;
+        }
+    }
+}
